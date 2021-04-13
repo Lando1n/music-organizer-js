38ea7e678e89d57936b43042d4b15e70edb313c2
Landon Martin <landon-martin@hotmail.com>
2021-04-12 23:03:36 -0400

    Starting the script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
 module.exports = {
-  extends: ["plugin:prettier/recommended"],
+  extends: ['plugin:prettier/recommended'],
   env: {
     browser: true,
     commonjs: true,
-    es2021: true,
+    es2021: true
   },
-  plugins: ["prettier"],
+  plugins: ['prettier'],
   globals: {},
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 2021
   },
   rules: {
-    "prettier/prettier": "error",
-    "new-cap": "off",
-    "require-jsdoc": "off",
-    "guard-for-in": "off",
-  },
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'none'
+      }
+    ],
+    'new-cap': 'off',
+    'require-jsdoc': 'off',
+    'guard-for-in': 'off',
+    quotes: [2, 'single', { avoidEscape: true }]
+  }
 };
---
+++ src/utils/getFilesRecursively.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+function getFilesRecursively(dirPath, arrayOfFiles, extensions) {
+  files = fs.readdirSync(dirPath);
+
+  files.forEach((file) => {
+    if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
+      arrayOfFiles = getFilesRecursively(path.join(dirPath, file), arrayOfFiles, extensions);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, file));
+    }
+  });
+
+  return arrayOfFiles.filter((filename) => extensions.includes(path.extname(filename)));
+}
+
+module.exports = getFilesRecursively;
