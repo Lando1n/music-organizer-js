edaec4ebbd99ef95d538b05ab74334ace0473bb3
Lando1n <landon-martin@hotmail.com>
2021-04-13 09:14:34 -0400

    Add autocomplete choices to paths, pull out sort function (#2)
    * Add autocomplete choices to paths
    
    * Generalize sort function
    
    * Add counter for songs moved

---
+++ src/utils/sort.js
@@ -0,0 +1,21 @@
+const mm = require('music-metadata');
+
+const getFilesRecursively = require('./getFilesRecursively');
+
+module.exports = (unsortedPath, sortedPath, extensions) => {
+  let movedFiles = 0;
+  const unsortedFiles = getFilesRecursively(unsortedPath, [], extensions);
+
+  unsortedFiles.forEach(async (startingLocation) => {
+    const metadata = await mm.parseFile(startingLocation);
+    const dir = path
+      .join(sortedPath, metadata.common.albumartist, metadata.common.album)
+      .replace(/[,-]/g, '');
+    fs.mkdirSync(dir, { recursive: true });
+    const newLocation = path.join(dir, path.basename(startingLocation));
+    fs.renameSync(startingLocation, newLocation);
+    movedFiles++;
+    console.log(`Moved ${startingLocation} to ${newLocation}`);
+  });
+  return movedFiles;
+};
