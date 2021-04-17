89c13dea89c275b0b4a9a61a54d8f815046eb7be
Landon Martin <landon-martin@hotmail.com>
2021-04-16 22:56:45 -0400

    Replace invalid symbols from filename

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -4,20 +4,45 @@ const fs = require('fs');
 
 const getFilesRecursively = require('./getFilesRecursively');
 
-module.exports = (unsortedPath, sortedPath, extensions) => {
+module.exports = async (unsortedPath, sortedPath, extensions) => {
   let movedFiles = 0;
   const unsortedFiles = getFilesRecursively(unsortedPath, [], extensions);
 
-  unsortedFiles.forEach(async (startingLocation) => {
+  for (const startingLocation of unsortedFiles) {
     const metadata = await mm.parseFile(startingLocation);
-    const dir = path
-      .join(sortedPath, metadata.common.albumartist, metadata.common.album)
-      .replace(/[,-]/g, '');
+    const artist = metadata.common.albumartist;
+    const album = metadata.common.album;
+    const title = metadata.common.title;
+    const ext = path.extname(startingLocation);
+
+    let pathParts = [artist, album, `${title}${ext}`];
+
+    // Remove invalid chars from paths
+    let replaceRegex;
+    switch (process.platform) {
+      case 'win32':
+        replaceRegex = /[<>:"/\\|?*]/g;
+        break;
+      case 'linux':
+        replaceRegex = /[/]/g;
+        break;
+      default:
+        throw Error('Only Linux and Windows are currently supported!');
+    }
+    pathParts = pathParts.map((part) => part.replace(replaceRegex, ''));
+
+    const newLocation = path.join(sortedPath, ...pathParts);
+    const dir = path.dirname(newLocation);
+    // Create a directory for the file to be moved
     fs.mkdirSync(dir, { recursive: true });
-    const newLocation = path.join(dir, path.basename(startingLocation));
-    fs.renameSync(startingLocation, newLocation);
+    // Attempt to move the file
+    try {
+      fs.renameSync(startingLocation, newLocation);
+    } catch (e) {
+      console.error(`Failed to move file: ${startingLocation}, Reason: ${e}`);
+    }
     movedFiles++;
     console.log(`Moved ${startingLocation} to ${newLocation}`);
-  });
+  }
   return movedFiles;
 };
