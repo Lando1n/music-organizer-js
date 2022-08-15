d5ef465dfbb4f29051357ec4eeaddf01bf1ed164
Landon Martin <landon-martin@hotmail.com>
2022-08-14 20:32:55 -0400

    Add ability to build os seperately

--- build.js
+++ build.js
@@ -1,26 +1,53 @@
 const { compile } = require('nexe');
 
-// Build linux
-compile({
-  input: './src/entrypoint.js',
-  target: 'linux-x64-14.15.3',
-  output: './dist/music-organizer-js'
-}).then(() => {
-  console.log('success');
-});
-// Build Windows
-compile({
-  input: './src/entrypoint.js',
-  target: 'windows-x64-14.5.0',
-  output: './dist/music-organizer-js'
-}).then(() => {
-  console.log('success');
-});
-// Build Mac
-compile({
-  input: './src/entrypoint.js',
-  target: 'mac-x64-14.15.3',
-  output: './dist/music-organizer-js-mac'
-}).then(() => {
-  console.log('success');
-});
+const arg = process.argv.slice(2)[0];
+
+switch (arg) {
+  case 'linux':
+    buildLinux();
+    break;
+  case 'windows':
+    buildWindows();
+    break;
+  case 'mac':
+    buildMac();
+    break;
+  default:
+    buildLinux();
+    buildWindows();
+    buildMac();
+    break;
+}
+
+function buildLinux() {
+  // Build linux
+  compile({
+    input: './src/entrypoint.js',
+    target: 'linux-x64-14.15.3',
+    output: './dist/music-organizer-js'
+  }).then(() => {
+    console.log('success');
+  });
+}
+
+function buildWindows() {
+  // Build Windows
+  compile({
+    input: './src/entrypoint.js',
+    target: 'windows-x64-14.5.0',
+    output: './dist/music-organizer-js'
+  }).then(() => {
+    console.log('success');
+  });
+}
+
+function buildMac() {
+  // Build Mac
+  compile({
+    input: './src/entrypoint.js',
+    target: 'mac-x64-14.15.3',
+    output: './dist/music-organizer-js-mac'
+  }).then(() => {
+    console.log('success');
+  });
+}
