ce8ce292a78422b177a6c3b4cc4eb5276686694b
Landon Martin <landon-martin@hotmail.com>
2022-09-17 13:45:58 -0400

    GH-31: Add logo and version when organizing

--- src/organize.js
+++ src/organize.js
@@ -1,10 +1,36 @@
+const chalk = require('chalk');
+
 const paths = require('./paths');
 const sort = require('./utils/sort');
 const Setttings = require('./utils/Settings');
 
 const { removeEmptyDirsRecursively } = require('./utils/files');
 
+function printTitle() {
+  console.log(
+    chalk.blue(`  
+  ╔╗              ╔╗     ╔╗     
+  ║║              ║║    ╔╝║     
+  ║║   ╔══╗ ╔═╗ ╔═╝║╔══╗╚╗║ ╔═╗ 
+  ║║ ╔╗╚ ╗║ ║╔╗╗║╔╗║║╔╗║ ║║ ║╔╗╗
+  ║╚═╝║║╚╝╚╗║║║║║╚╝║║╚╝║╔╝╚╗║║║║
+  ╚═══╝╚═══╝╚╝╚╝╚══╝╚══╝╚══╝╚╝╚╝`)
+  );
+  console.log(
+    chalk.rgb(
+      255,
+      165,
+      0
+    )(`
+  MUSIC ORGANIZER JS, VERSION: ${require('../package.json').version}
+  ------------------------------------
+  `)
+  );
+}
+
 async function organize() {
+  printTitle();
+
   const settings = new Setttings(paths.setupSettings);
 
   if (!settings.exists() || !settings.validate()) {
